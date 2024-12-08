e132d29ecc21ffcdc60916e6b679c189a5d12b52
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-12-08 00:14:44 -0300

    fix5

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  output: 'export', // This is used for static export
+  distDir: 'dist',
+  reactStrictMode: true,
   i18n: {
-    output: 'export', // This is used for static export
-    distDir: 'dist',
-    reactStrictMode: true,
     locales: ['en', 'pt-br'],
     defaultLocale: 'en',
   },
